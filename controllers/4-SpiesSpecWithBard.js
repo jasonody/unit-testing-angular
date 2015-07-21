a8ae0da85c45611981936110210415f2726f02b0
Jason Ody <jasonody@users.noreply.github.com>
2015-07-20 17:07:45 -0700

    Testing a controller using bard.js

--- controllers/4-SpiesSpecWithBard.js
+++ controllers/4-SpiesSpecWithBard.js
@@ -1,4 +1,38 @@
 describe('SpiesController using Bard', function () {
-	
-	
+
+	var controller;
+
+	beforeEach(function () {
+
+		bard.appModule('testingAngular.controllers')
+		bard.inject('$controller');
+
+		controller = $controller("SpiesController");
+	});
+
+	describe('isStaff', function () {
+
+		beforeEach(function () {
+
+			bard.inject('user');
+		});
+
+		it('should return true if staff', function () {
+
+			spyOn(user, 'getUserType').and.returnValue('Staff');
+
+			var result = controller.isStaff();
+
+			expect(result).toBeTruthy();
+		});
+
+		it('should return false if not staff', function () {
+
+			spyOn(user, 'getUserType').and.returnValue('Not staff');
+
+			var result = controller.isStaff();
+
+			expect(result).toBeFalsy();
+		});
+	});
 });
